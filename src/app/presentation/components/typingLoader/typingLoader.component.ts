60f3e3f8ec525a21edc2f9a8b05f0be57f1a481f
Francisco Lino <franksuriel_7@hotmail.com>
2024-01-16 22:09:01 -0600

    ADD typing loader

---
+++ src/app/presentation/components/typingLoader/typingLoader.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typing-loader',
+  standalone: true,
+  imports: [],
+  styleUrl: './typingLoader.component.css',
+  template: `
+    <div class="typing">
+      <span class="circle scaling"></span>
+      <span class="circle scaling"></span>
+      <span class="circle scaling"></span>
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TypingLoaderComponent {}
