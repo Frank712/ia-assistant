d38e67bbc0c4fd885fc4d3853b1914b512b07df3
Francisco Lino <franksuriel_7@hotmail.com>
2024-01-16 22:10:31 -0600

    ADD orthographyPage

--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,12 +1,52 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ChatMessageComponent, MyMessageComponent } from '@components/index';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import {
+  ChatMessageComponent,
+  MyMessageComponent,
+  TextBoxMessageSelectComponent,
+  TextMessageBoxComponent,
+  TextMessageBoxFileComponent,
+  TextMessageBoxSelectedEvent,
+  TextMessageEvent,
+  TypingLoaderComponent,
+} from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-orthography-page',
   standalone: true,
-  imports: [CommonModule, ChatMessageComponent, MyMessageComponent],
+  imports: [
+    CommonModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextBoxMessageSelectComponent,
+    TextMessageBoxComponent,
+    TextMessageBoxFileComponent,
+  ],
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class OrthographyPageComponent {}
+export default class OrthographyPageComponent {
+  public messages = signal<Message[]>([{ text: 'Hello world', isGpt: false }]);
+  public isLoading = signal(false);
+  public openAiService = inject(OpenAiService);
+
+  handlerMessage(event: string) {
+    console.log(event);
+  }
+
+  handlerMessageWithFile({ prompt, file }: TextMessageEvent) {
+    console.log({ prompt, file });
+  }
+
+  handlerMessageWithSelect(event: TextMessageBoxSelectedEvent) {
+    console.log({ event });
+  }
+}
