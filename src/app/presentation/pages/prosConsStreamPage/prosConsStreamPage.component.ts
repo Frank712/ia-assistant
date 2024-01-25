1e8d31b8e80ffedb63dd7128226c72f302d5343e
Francisco Lino <franksuriel_7@hotmail.com>
2024-01-25 13:54:35 -0600

    ADD ProsConsStreamPageComponent component

--- src/app/presentation/pages/prosConsStreamPage/prosConsStreamPage.component.ts
+++ src/app/presentation/pages/prosConsStreamPage/prosConsStreamPage.component.ts
@@ -1,11 +1,61 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import {
+  ChatMessageComponent,
+  MyMessageComponent,
+  TextMessageBoxComponent,
+  TypingLoaderComponent,
+} from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-pros-cons-stream-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TextMessageBoxComponent,
+    TypingLoaderComponent,
+  ],
   templateUrl: './prosConsStreamPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ProsConsStreamPageComponent {}
+export default class ProsConsStreamPageComponent {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public openAiService = inject(OpenAiService);
+  public abortSignal = new AbortController();
+
+  async handlerMessage(prompt: string) {
+    this.abortSignal.abort();
+    this.abortSignal = new AbortController();
+
+    this.isLoading.set(true);
+    this.messages.update((prev) => [
+      ...prev,
+      { isGpt: false, text: prompt },
+      { isGpt: true, text: '...' },
+    ]);
+    const stream = this.openAiService.prosConsDiscusserStream(
+      prompt,
+      this.abortSignal.signal
+    );
+    this.isLoading.set(false);
+    for await (const text of stream) {
+      this.handleStreamResponse(text);
+    }
+  }
+
+  handleStreamResponse(message: string) {
+    this.messages().pop();
+    const messages = this.messages();
+    this.messages.set([...messages, { isGpt: true, text: message }]);
+  }
+}
